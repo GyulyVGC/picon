cedadbe5f118ddd1a0f2104ea841a93617fed55c
GyulyVGC <gyulyvgc99@gmail.com>
2026-03-01 14:06:07 +0100

    Windows implementation attempt using windows-icons crate

--- src/platform/windows/mod.rs
+++ src/platform/windows/mod.rs
@@ -14,8 +14,12 @@ pub(crate) fn get_icon_by_path(path: String) -> Option<Icon> {
     let _ = std::fs::create_dir(format!("{manifest_dir}/output"));
     let icon = windows_icons::get_icon_by_path(&path);
     if let Ok(icon) = icon {
+        let file_name = std::path::Path::new(&path)
+            .file_stem()
+            .and_then(|n| n.to_str())
+            .unwrap_or("unknown");
         println!("Successfully extracted icon for {path}");
-        icon.save(format!("{manifest_dir}/output/ciao.png")).unwrap();
+        icon.save(format!("{manifest_dir}/output/{file_name}.png")).unwrap();
         return Some(Icon::new(icon.into_raw()));
     }
     None
