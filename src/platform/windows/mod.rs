d26b4f40550a200c43ced7c7c6f709266c3f7f29
GyulyVGC <gyulyvgc99@gmail.com>
2026-03-01 13:16:58 +0100

    Windows implementation attempt using windows-icons crate

--- src/platform/windows/mod.rs
+++ src/platform/windows/mod.rs
@@ -1,116 +1,105 @@
 use crate::Icon;
-use std::ffi::OsStr;
-use std::os::windows::ffi::OsStrExt;
-use std::path::Path;
-
-use windows::{
-    core::PCWSTR,
-    Win32::{
-        Foundation::{HWND},
-        Graphics::Gdi::{
-            BITMAP, BITMAPINFO, BITMAPINFOHEADER, BI_RGB, DIB_RGB_COLORS,
-            DeleteObject, GetDIBits, GetObjectW,
-        },
-        UI::{
-            Shell::{SHGetFileInfoW, SHFILEINFOW, SHGFI_ICON, SHGFI_LARGEICON},
-            WindowsAndMessaging::{
-                DestroyIcon, GetDC, GetIconInfo, ReleaseDC,
-                HICON, ICONINFO,
-            },
-        },
-    },
-};
+// use winapi::um::wingdi::{ CreateCompatibleDC, DeleteDC, DeleteObject, GetDIBits, BITMAPINFO, BITMAPINFOHEADER, BI_RGB, RGBQUAD };
+// use winapi::um::shellapi::{ SHGetFileInfoW, SHFILEINFOW, SHGFI_ICON, SHGFI_LARGEICON };
+// use winapi::um::winbase::{ GlobalAlloc, GlobalLock, GHND, GlobalUnlock, GlobalFree };
+// use winapi::um::winuser::{ GetIconInfo, ICONINFO, DestroyIcon };
+// use winapi::shared::minwindef::DWORD;
+// use iced::widget::image::Handle;
+// use widestring::U16CString;
+// use std::{mem, ptr, slice};
 
 pub(crate) fn get_icon_by_path(path: String) -> Option<Icon> {
-    get_icon_bytes(&path).map(Icon::new)
+    windows_icons::get_icon_base64_by_path(path).ok().map(Icon::new)
 }
 
-fn get_icon_bytes(path: &Path) -> Option<Vec<u8>> {
-    unsafe {
-        // UTF-16 path
-        let wide: Vec<u16> = OsStr::new(path)
-            .encode_wide()
-            .chain(std::iter::once(0))
-            .collect();
-
-        let mut file_info = SHFILEINFOW::default();
-
-        let res = SHGetFileInfoW(
-            PCWSTR(wide.as_ptr()),
-            0,
-            Some(&mut file_info),
-            std::mem::size_of::<SHFILEINFOW>() as u32,
-            SHGFI_ICON | SHGFI_LARGEICON,
-        );
-
-        if res == 0 {
-            return None;
-        }
-
-        let hicon: HICON = file_info.hIcon;
-        if hicon.is_invalid() {
-            return None;
-        }
-
-        // Extract bitmap from icon
-        let mut icon_info = ICONINFO::default();
-        if !GetIconInfo(hicon, &mut icon_info).as_bool() {
-            DestroyIcon(hicon);
-            return None;
-        }
-
-        let mut bmp = BITMAP::default();
-        if GetObjectW(
-            icon_info.hbmColor,
-            std::mem::size_of::<BITMAP>() as i32,
-            Some(&mut bmp as *mut _ as *mut _),
-        ) == 0
-        {
-            DestroyIcon(hicon);
-            return None;
-        }
-
-        let width = bmp.bmWidth;
-        let height = bmp.bmHeight;
-
-        let mut buffer = vec![0u8; (width * height * 4) as usize];
-
-        let hdc = GetDC(HWND(0));
-
-        let mut bmi = BITMAPINFO {
-            bmiHeader: BITMAPINFOHEADER {
-                biSize: std::mem::size_of::<BITMAPINFOHEADER>() as u32,
-                biWidth: width,
-                biHeight: -height, // top-down
-                biPlanes: 1,
-                biBitCount: 32,
-                biCompression: BI_RGB.0 as u32,
-                ..Default::default()
-            },
-            ..Default::default()
-        };
-
-        let result = GetDIBits(
-            hdc,
-            icon_info.hbmColor,
-            0,
-            height as u32,
-            Some(buffer.as_mut_ptr() as *mut _),
-            &mut bmi,
-            DIB_RGB_COLORS,
-        );
-
-        ReleaseDC(HWND(0), hdc);
-
-        DeleteObject(icon_info.hbmColor);
-        DeleteObject(icon_info.hbmMask);
-        DestroyIcon(hicon);
-
-        if result == 0 {
-            return None;
-        }
-
-        Some(buffer)
-    }
-}
+// pub fn extract_icon_as_handle(path: &str) -> Result<Handle, Box<dyn std::error::Error>> {
+//     unsafe {
+//         let mut shfi = SHFILEINFOW {
+//             hIcon: ptr::null_mut(), iIcon: 0,
+//             dwAttributes: 0,
+//             szDisplayName: [0; 260],
+//             szTypeName: [0; 80],
+//         };
+//
+//         SHGetFileInfoW(
+//             U16CString::from_str(path)?.as_ptr(),
+//             0,
+//             &mut shfi,
+//             mem::size_of::<SHFILEINFOW>() as DWORD,
+//             SHGFI_ICON | SHGFI_LARGEICON,
+//         );
+//
+//         if shfi.hIcon.is_null() {
+//             return Err("No icon found.".into());
+//         }
+//
+//         let mut icon_info = ICONINFO {
+//             fIcon: 0,
+//             xHotspot: 0,
+//             yHotspot: 0,
+//             hbmMask: ptr::null_mut(),
+//             hbmColor: ptr::null_mut(),
+//         };
+//         GetIconInfo(shfi.hIcon, &mut icon_info);
+//
+//         let hdc = CreateCompatibleDC(ptr::null_mut());
+//
+//         let bmp_info_header = BITMAPINFOHEADER {
+//             biSize: mem::size_of::<BITMAPINFOHEADER>() as DWORD,
+//             biWidth: 32,
+//             biHeight: -32, // Negative to indicate a top-down DIB
+//             biPlanes: 1,
+//             biBitCount: 32,
+//             biCompression: BI_RGB as DWORD,
+//             biSizeImage: 0,
+//             biXPelsPerMeter: 0,
+//             biYPelsPerMeter: 0,
+//             biClrUsed: 0,
+//             biClrImportant: 0,
+//         };
+//
+//         let mut bitmap_info = BITMAPINFO {
+//             bmiHeader: bmp_info_header,
+//             bmiColors: [RGBQUAD { rgbBlue: 0, rgbGreen: 0, rgbRed: 0, rgbReserved: 0 }; 1],
+//         };
+//
+//         let bitmap_memory = GlobalAlloc(GHND, (32 * 32 * 4) as usize);
+//         let bitmap_bits = GlobalLock(bitmap_memory) as *mut u8;
+//
+//         GetDIBits(
+//             hdc,
+//             icon_info.hbmColor,
+//             0,
+//             32,
+//             bitmap_bits as *mut _,
+//             &mut bitmap_info,
+//             0,
+//         );
+//
+//         GlobalUnlock(bitmap_memory);
+//         DeleteDC(hdc);
+//         DeleteObject(icon_info.hbmColor as _);
+//         DestroyIcon(shfi.hIcon);
+//
+//         let bitmap_slice = slice::from_raw_parts(bitmap_bits, (32 * 32 * 4) as usize).to_vec();
+//
+//         let mut rgba_slice = vec![0u8; bitmap_slice.len()];
+//         for i in 0..(32 * 32) {
+//             let b = bitmap_slice[i * 4 + 0];
+//             let g = bitmap_slice[i * 4 + 1];
+//             let r = bitmap_slice[i * 4 + 2];
+//             let a = bitmap_slice[i * 4 + 3];
+//             rgba_slice[i * 4 + 0] = r;
+//             rgba_slice[i * 4 + 1] = g;
+//             rgba_slice[i * 4 + 2] = b;
+//             rgba_slice[i * 4 + 3] = a;
+//         }
+//
+//         GlobalFree(bitmap_memory);
+//
+//         let handle = Handle::from_pixels(32, 32, rgba_slice);
+//
+//         return Ok(handle);
+//     }
+// }
 
