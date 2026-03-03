4739af32773e9d1f441eb7848c0743d07a6132a1
GyulyVGC <gyulyvgc99@gmail.com>
2026-03-03 18:24:43 +0100

    use program name on linux

--- src/platform/linux/mod.rs
+++ src/platform/linux/mod.rs
@@ -2,10 +2,7 @@ use crate::IconHandle;
 use std::fs;
 use std::path::{Path, PathBuf};
 
-pub(crate) fn get_icon(program: String) -> Option<IconHandle> {
-    let path = Path::new(&path);
-    let name = path.file_stem()?.to_string_lossy().to_string();
-
+pub(crate) fn get_icon(name: String) -> Option<IconHandle> {
     let icons = icon::Icons::new();
     let icon_opt = icons.find_default_icon(&name, 64, 1);
 
