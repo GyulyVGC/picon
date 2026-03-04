575c3dc55177ed06bc2aeb63d99e5cbf0746eec0
GyulyVGC <gyulyvgc99@gmail.com>
2026-03-04 18:47:07 +0100

    improve desktop entries icon parsing on linux

--- src/platform/linux/mod.rs
+++ src/platform/linux/mod.rs
@@ -6,6 +6,21 @@ pub(crate) fn get_icon(name: String) -> Option<IconHandle> {
     let icon_name_opt = find_icon_name(&name);
     let icon_name = icon_name_opt.unwrap_or(name);
 
+    // first check if the icon name is an absolute path to an icon file
+    let icon_path = PathBuf::new(&icon_name);
+    if icon_path.is_absolute() {
+        return match icon_path.extension() {
+            Some(ext) if ext == "png" => Some(IconHandle::Image(
+                iced::widget::image::Handle::from_path(icon_path),
+            )),
+            Some(ext) if ext == "svg" => Some(IconHandle::Svg(
+                iced::widget::svg::Handle::from_path(icon_path),
+            )),
+            _ => None,
+        };
+    }
+
+    // try to find the icon using the icon crate
     let icons = icon::Icons::new();
     let icon_opt = icons.find_default_icon(&icon_name, 64, 1);
 
@@ -21,31 +36,38 @@ pub(crate) fn get_icon(name: String) -> Option<IconHandle> {
             _ => None,
         };
     }
+
     None
 }
 
 fn find_icon_name(name: &str) -> Option<String> {
-    let mut ret_val = None;
-    let mut found = false;
-
-    let dirs = [
-        PathBuf::from("/usr/share/applications"),
-        PathBuf::from("/usr/local/share/applications"),
-        PathBuf::from(shellexpand::tilde("~/.local/share/applications").into_owned()),
-    ];
+    let mut dirs = Vec::new();
+    if let Some(home_dir) = dirs::home_dir().map(|p| p.to_string_lossy().into_owned()) {
+        println!("Full home icon path: {home_dir}/.local/share/applications");
+        dirs.push(PathBuf::from(&format!(
+            "{home_dir}/.local/share/applications"
+        )));
+    }
+    dirs.push(PathBuf::from("/usr/share/applications"));
+    dirs.push(PathBuf::from("/usr/local/share/applications"));
 
     let entries = dirs
         .into_iter()
         .filter_map(|dir| fs::read_dir(dir).ok())
         .flat_map(|rd| rd.filter_map(Result::ok));
 
+    let mut ret_val = None;
+    let mut found = false;
+
     for entry in entries {
         let path = entry.path();
         if path.extension().and_then(|ext| ext.to_str()) == Some("desktop") {
             if let Ok(content) = fs::read_to_string(&path) {
                 for line in content.lines() {
-                    if let Some(icon_name) = line.strip_prefix("Icon=")
-                        && !icon_name.trim().is_empty()
+                    if let Some(icon_name) = line
+                        .strip_prefix("Icon=")
+                        .map(|s| s.replace('\"', "").replace('\\', "").trim())
+                        && !icon_name.is_empty()
                     {
                         ret_val = Some(icon_name.to_string());
                     }
