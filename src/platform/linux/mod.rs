845c13e691457c270b7eb7db6f013b7362301634
GyulyVGC <gyulyvgc99@gmail.com>
2026-03-04 19:20:09 +0100

    fix clippy linux

--- src/platform/linux/mod.rs
+++ src/platform/linux/mod.rs
@@ -33,7 +33,7 @@ pub(crate) fn get_icon(name: String) -> Option<IconHandle> {
             icon::FileType::Svg => {
                 Some(IconHandle::Svg(iced::widget::svg::Handle::from_path(path)))
             }
-            _ => None,
+            icon::FileType::Xpm => None,
         };
     }
 
@@ -60,35 +60,35 @@ fn find_icon_name(name: &str) -> Option<String> {
 
     for entry in entries {
         let path = entry.path();
-        if path.extension().and_then(|ext| ext.to_str()) == Some("desktop") {
-            if let Ok(content) = fs::read_to_string(&path) {
-                for line in content.lines() {
-                    if let Some(icon_name) = line
-                        .strip_prefix("Icon=")
-                        .map(|s| s.replace('\"', "").replace('\\', "").trim().to_string())
-                        && !icon_name.is_empty()
-                    {
-                        ret_val = Some(icon_name);
-                    }
-                    if let Some(exec_cmd) = line
-                        .strip_prefix("Exec=")
-                        .map(|s| s.replace('\"', "").replace('\\', "").trim().to_string())
-                    {
-                        let parts: Vec<&str> = exec_cmd.split_whitespace().collect();
+        if path.extension().and_then(|ext| ext.to_str()) == Some("desktop")
+            && let Ok(content) = fs::read_to_string(&path)
+        {
+            for line in content.lines() {
+                if let Some(icon_name) = line
+                    .strip_prefix("Icon=")
+                    .map(|s| s.replace(['\"', '\\'], "").trim().to_string())
+                    && !icon_name.is_empty()
+                {
+                    ret_val = Some(icon_name);
+                }
+                if let Some(exec_cmd) = line
+                    .strip_prefix("Exec=")
+                    .map(|s| s.replace(['\"', '\\'], "").trim().to_string())
+                {
+                    let parts: Vec<&str> = exec_cmd.split_whitespace().collect();
 
-                        if parts.iter().any(|part| {
-                            let part_name = Path::new(part)
-                                .file_name()
-                                .and_then(|s| s.to_str())
-                                .unwrap_or("");
-                            if name.len() < 15 {
-                                part_name == name
-                            } else {
-                                part_name.contains(name)
-                            }
-                        }) {
-                            found = true;
+                    if parts.iter().any(|part| {
+                        let part_name = Path::new(part)
+                            .file_name()
+                            .and_then(|s| s.to_str())
+                            .unwrap_or("");
+                        if name.len() < 15 {
+                            part_name == name
+                        } else {
+                            part_name.contains(name)
                         }
+                    }) {
+                        found = true;
                     }
                 }
             }
